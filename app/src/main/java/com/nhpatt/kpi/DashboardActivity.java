f75c6af678b0aa14f7f60fc16789541f10f59169
Javier Gamarra <nhpatt@gmail.com>
2015-09-12 07:24:41 +0200

    logging a message

--- app/src/main/java/com/nhpatt/kpi/DashboardActivity.java
+++ app/src/main/java/com/nhpatt/kpi/DashboardActivity.java
@@ -2,6 +2,7 @@ package com.nhpatt.kpi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,8 @@ public class DashboardActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
+
+		Log.d("KPI", "Starting app...");
 	}
 
 	@Override
