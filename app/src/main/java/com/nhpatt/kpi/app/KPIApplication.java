a613ea9c57bed2ffa8a9c8d50b62ccfed7dd9ba2
Javier Gamarra <nhpatt@gmail.com>
2015-10-31 07:49:30 +0100

    application class

---
+++ app/src/main/java/com/nhpatt/kpi/app/KPIApplication.java
@@ -0,0 +1,14 @@
+package com.nhpatt.kpi.app;
+
+import android.app.Application;
+
+/**
+ * @author Javier Gamarra
+ */
+public class KPIApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
